3b99bf2298c4ed4a0215976fcdbd55552e6a3f21
Siluni Rupasinghe <silunirupasinghe@gmail.com>
2024-07-19 22:14:10 +0530

    error handling

--- files/aidf-back-end/src/api/middleware/global.error.handler.ts
+++ files/aidf-back-end/src/api/middleware/global.error.handler.ts
@@ -2,6 +2,7 @@ import { Response,Request , NextFunction} from "express";
 
 const GlobalErrorHandlingMiddleware= (req:Request, res: Response, next:NextFunction) =>{
 1.05.09
+
 }
 
 export default GlobalErrorHandlingMiddleware
